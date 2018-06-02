22bf148056a44c18435efaefece217f18c3039eb
Daphnae <vidaldaph@gmail.com>
2018-06-02 04:27:22 -0400

    Primer commit

---
+++ Booleanos.py
@@ -0,0 +1,6 @@
+# "*" coding: iso-8859-15 "*
+import os, sys
+valor= 6
+a=int(input("Ingrese numero: "))
+if a==valor:
+    print valor, "es igual"
---
+++ suma.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python
+# "*" coding: iso-8859-15 "*
+import os, sys
+a= int(input("Unidades :"))
+b= int(input("precio :"))
+print("A pagar=", a*b)
